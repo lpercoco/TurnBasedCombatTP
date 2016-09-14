dcb591fd973f0297d6744dd87350962da190cf81
rdjah <lisandro.percoco@hotmail.com>
2016-09-14 11:03:45 -0300

    Nuevos metodos
    agrege neuvos metodos update,add,delete,getbynombre al controlador de
    crud de personajes

--- src/negocio/ctrlPersonajes.java
+++ src/negocio/ctrlPersonajes.java
@@ -1,11 +1,30 @@
 package negocio;
 
-import java.util.ArrayList;
+import data.DataPersonaje;
 import entidades.Personaje;
 
 public class ctrlPersonajes {	
-	// metodo agregar personaje	
-	// metodo modificar personaje
-	// metodo eliminar personaje
-	// metodo buscar personaje	
+	private data.DataPersonaje dataP;
+	
+	public ctrlPersonajes(){
+		dataP=new DataPersonaje();
+	}
+	
+	public void add(Personaje p) {
+		dataP.add(p);		
+	}
+
+	public void update(Personaje p){
+		dataP.update(p);
+	}
+	
+	public Personaje getByNombre(Personaje per){
+		Personaje p = new Personaje();
+		p=dataP.getByNombre(per);
+		return p;
+	}
+	
+	public void delete(Personaje p){
+		dataP.delete(p);
+	}
 }
